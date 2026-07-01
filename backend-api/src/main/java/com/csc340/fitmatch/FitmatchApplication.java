adc8dc0a33d8f8f19c3eddc8f7704ebb7b71f3cf
csc372uncg <csc372.uncg@gmail.com>
2026-06-30 21:28:00 -0400

    added starter files for project API

---
+++ backend-api/src/main/java/com/csc340/fitmatch/FitmatchApplication.java
@@ -0,0 +1,13 @@
+package com.csc340.fitmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitmatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitmatchApplication.class, args);
+	}
+
+}
---
+++ backend-api/src/test/java/com/csc340/fitmatch/FitmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csc340.fitmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
