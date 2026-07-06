d5df9ee5cfc659f2f2ff0f71a0917f661be2237c
csc372uncg <csc372.uncg@gmail.com>
2026-07-05 22:04:10 -0400

    fix: Add @RequestBody annotation to bookTrainingSession method for proper request handling

--- backend-api/src/main/java/com/csc340/fitmatch/controller/TrainingSessionController.java
+++ backend-api/src/main/java/com/csc340/fitmatch/controller/TrainingSessionController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +24,7 @@ public class TrainingSessionController {
   }
 
   @PostMapping
-  public ResponseEntity<TrainingSession> bookTrainingSession(TrainingSession trainingSession) {
+  public ResponseEntity<TrainingSession> bookTrainingSession(@RequestBody TrainingSession trainingSession) {
     TrainingSession createdSession = trainingSessionService.createTrainingSession(trainingSession);
     return ResponseEntity.ok(createdSession);
   }
