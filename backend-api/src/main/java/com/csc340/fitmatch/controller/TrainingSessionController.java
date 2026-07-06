77fd2fed80abdca3ee7a62ebb2768b711a8a65bf
csc372uncg <csc372.uncg@gmail.com>
2026-07-05 21:32:22 -0400

    feat: Add booking endpoint for training sessions in TrainingSessionController

--- backend-api/src/main/java/com/csc340/fitmatch/controller/TrainingSessionController.java
+++ backend-api/src/main/java/com/csc340/fitmatch/controller/TrainingSessionController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,12 @@ public class TrainingSessionController {
 
   public TrainingSessionController(TrainingSessionService trainingSessionService) {
     this.trainingSessionService = trainingSessionService;
+  }
+
+  @PostMapping
+  public ResponseEntity<TrainingSession> bookTrainingSession(TrainingSession trainingSession) {
+    TrainingSession createdSession = trainingSessionService.createTrainingSession(trainingSession);
+    return ResponseEntity.ok(createdSession);
   }
 
   @GetMapping("/customer/{customerId}")
