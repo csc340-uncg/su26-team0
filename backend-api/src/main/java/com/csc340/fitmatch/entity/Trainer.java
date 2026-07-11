80be251675db9e955d71dc5c5f667be316271333
csc372uncg <csc372.uncg@gmail.com>
2026-07-11 02:30:54 -0400

    fix: Add fetch type to profilePicture field in Trainer entity

--- backend-api/src/main/java/com/csc340/fitmatch/entity/Trainer.java
+++ backend-api/src/main/java/com/csc340/fitmatch/entity/Trainer.java
@@ -6,8 +6,10 @@ import org.hibernate.annotations.JdbcTypeCode;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import jakarta.persistence.Basic;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -44,6 +46,7 @@ public class Trainer {
   @Lob
   @JdbcTypeCode(java.sql.Types.BINARY)
   @Column(columnDefinition = "bytea")
+  @Basic(fetch = FetchType.LAZY)
   private byte[] profilePicture;
 
   @Column(columnDefinition = "TEXT")
